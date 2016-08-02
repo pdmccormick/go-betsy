8b12288c3a59d1c819e369032cf4f04db3366b58
Peter McCormick <peter@pdmccormick.com>
2016-08-02 00:44:29 -0400

    Load JSON tilemap inventory and map into display

---
+++ betsy/inventory.go
@@ -0,0 +1,44 @@
+package betsy
+
+import (
+	"encoding/json"
+	"net"
+	"os"
+)
+
+type Inventory struct {
+	Tilemap []struct {
+		IpLinkLocal string    `json:"ipv6_link_local"`
+		Start       []float64 `json:"start"`
+	} `json:"tilemap"`
+}
+
+func LoadTilemapInventory(filename string, disp *Display) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	inv := &Inventory{}
+	if err := json.NewDecoder(file).Decode(inv); err != nil {
+		return err
+	}
+
+	for _, tilemap := range inv.Tilemap {
+		ip := net.ParseIP(tilemap.IpLinkLocal)
+		tile, err := disp.Net.MakeTile(ip)
+		if err != nil {
+			return err
+		}
+
+		startX := int(tilemap.Start[0])
+		startY := int(tilemap.Start[1])
+
+		disp.MapTile(tile, startX, startY)
+	}
+
+	disp.SortMapping()
+
+	return nil
+}
